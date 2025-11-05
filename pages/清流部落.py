402a843a5146e4b6d65ac17eaf0a4e4d3458a67f
8048-kh <8046khtotn@gmail.com>
2025-11-05 17:44:17 +0800

    Update 清流部落.py

--- pages/清流部落.py
+++ pages/清流部落.py
@@ -1,41 +1,12 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
-import pandas as pd
 
-st.set_page_config(layout="wide")
+st.title("清流部落（川中島）")
+st.subheader("歷史背景")
+st.write("""
+清流部落（日人稱為川中島）位於北港溪流域台地，  
+1931年霧社事件後，日本政府強制將抗日六部落餘生者遷居於此，  
+族人以其三面臨水之地勢，稱此地為「gluban」，意指中途休息之地。
+""")
 
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
-
-st.title("原鄉部落座標與資訊")
-
-# Create a Leafmap map object
-m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
-
-# Load the tribes data
-tribes = "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/Nantou_Tribe.csv"
-tribes_df = pd.read_csv(tribes)
-tribe_names = tribes_df['tribe name'].tolist()
-
-# Add points to the map
-# Create a selectbox for tribe names
-selected_tribe = st.selectbox(
-    "選擇部落", tribe_names, key="selectbox_tribe"
-)
-
-# Get the data of the selected tribe
-selected_tribe_data = tribes_df[tribes_df['tribe name'] == selected_tribe].iloc[0]
-
-# Get coordinates from 'latitude' and 'longitude'
-latitude = selected_tribe_data['latitude']
-longitude = selected_tribe_data['longitude']
-m.add_shp("https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/Full_Nantou_Tribe.shp")
-# Recenter and zoom to the selected tribe
-m.set_center(longitude, latitude, zoom=15) 
-m.add_marker(location=(latitude, longitude), tooltip=selected_tribe, popup=f"{selected_tribe}")
-# Display the map in Streamlit
-st.write(f"您選擇的部落是：{selected_tribe}")
-st.table(selected_tribe_data)
-m.to_streamlit(height=700)
+st.subheader("地理位置")
+st.map(data={"lat": [24.0], "lon": [121.1]}, zoom=12)
