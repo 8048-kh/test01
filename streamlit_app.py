a9dce4ff13ead51487aa50f65c177bb2f590b1f1
8048-kh <8046khtotn@gmail.com>
2025-11-05 18:13:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,10 @@
 import streamlit as st
-import pandas as pd
+import leafmap.foliumap as leafmap
 
-st.set_page_config(layout="wide", page_title="南投原鄉部落與土石流分布")
+st.set_page_config(layout="wide")
 
-# Sidebar
-st.sidebar.title("About")
-st.sidebar.info("""
-Web App URL: <https://blank-app-qc2181tdcxg.streamlit.app/>
-GitHub Repository: <https://github.com/8048-kh/test01/tree/main>
-""")
+if "map" not in st.session_state:
+    m = leafmap.Map(center=[23.932630, 120.986852], zoom=10)
+    st.session_state["map"] = m
 
-# Main content
-st.title("南投原鄉部落與土石流分布")
-st.markdown("南投原鄉部落與土石流潛勢溪流、土石流潛勢溪流範圍分布")
-
-st.header("目錄")
-st.markdown("1.清流部落")
-
-# 顯示部落表格
-tribes_df = pd.read_csv(
-    "https://github.com/8048-kh/Debris-rep/raw/refs/heads/master/Data/Nantou_Tribe.csv"
-)
-st.header("部落名稱")
-st.table(tribes_df[["tribe name"]])
+st.session_state["map"].to_streamlit(height=600)
