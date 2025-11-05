80192468dc070bb25495eb67734d59fda07d9728
8048-kh <8046khtotn@gmail.com>
2025-11-05 17:24:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+pip install leafmap
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
